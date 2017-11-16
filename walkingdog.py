d1aaa2fcf0d1ca699f23804ff6a1812df2e6c968
jboas <jboas@sjnma.org>
2017-11-16 14:12:50 -0600

    walk

--- walkingdog.py
+++ walkingdog.py
@@ -35,12 +35,25 @@ pet2 = [
 	]
 
 def walking():
-	for i in range(10):
-		sense.set_pixels(pet1)
-		time.sleep(0.5)
-		sense.set_pixels(pet2)
-		time.sleep(0.5)
-  
+
+	sense.set_pixels(pet1)
+	time.sleep(0.5)
+	sense.set_pixels(pet2)
+	time.sleep(0.5)
+  	while x<.05 or x>.05 or y<.05 or y>.05 or z<.05 or z>.05:
+
+	        RED = [
+        	        R, R, R, R, R, R, R, R,
+        	        R, R, R, R, R, R, R, R,
+        	        R, R, R, R, R, R, R, R,
+        	        R, R, R, R, R, R, R, R,
+                	R, R, R, R, R, R, R, R,
+			R, R, R, R, R, R, R, R,
+        	        R, R, R, R, R, R, R, R,
+        	        R, R, R, R, R, R, R, R,
+        	        ]
+
+
   
 sense.clear()
 
@@ -49,3 +62,17 @@ x, y, z = sense.get_accelerometer_raw().values()
 while x<2 and y<2 and z<2:
 	x, y, z = sense.get_accelerometer_raw().values()
 	walking()
+
+while x<0 or x>0 or y<0 or y>0 or z<0 or z>0:
+	
+	RED = [
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+        	R, R, R, R, R, R, R, R,
+	        ]
+
