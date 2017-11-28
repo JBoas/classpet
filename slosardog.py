83e8aa5699e041cf1e4949a57629f92803d5616f
jboas <jboas@sjnma.org>
2017-11-28 14:14:23 -0600

    pet

--- slosardog.py
+++ slosardog.py
@@ -8,6 +8,7 @@ g = (0, 102, 102)
 w = (200, 200, 200)
 y = (204, 204, 0)
 e = (0, 0, 0)
+r = (255, 0, 0)
 
 pet1 = [
 	e, e, e, e, e, e, e, e,
@@ -20,15 +21,15 @@ pet1 = [
 	e, e, e, e, e, e, e, e
 	]
 
-pet2 = [
-	e, e, e, e, e, e, e, e,
-	p, e, e, e, e, e, e, e,
-	e, p, e, e, p, e, p, e,
-	e, p, g, g, p, y, y, e,
-	e, p, g, g, g, y, y, g,
-	e, g, g, g, g, y, y, e,
-	e, g, e, g, e, g, e, e,
-	e, e, e, e, e, e, e, e,
+red = [
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r,
+	r, r, r, r, r, r, r, r
 	]
 def walking():
 	for i in range(10):
@@ -39,10 +40,9 @@ def walking():
 
 sense.clear()
 
-x, y, z = sense.get_accelerometer_raw().values()
-
-while x<2 and y<2 and z<2:
-	x, y, z = sense.get_accelerometer_raw().values()
-else:
-	sens.set_pixels(pet1 + pet2)
-	walking()
+while True:
+	sense.set_pixels(pet1)
+	x,y,z = sense.get_accelerometer_raw().values()
+	if(x > 2 or y > 2 or z > 2):
+		sense.set_pixels(red)
+	time.sleep(1)
--- walkingdog.py
+++ walkingdog.py
@@ -9,20 +9,24 @@ R = (255, 0, 0)
 W = (255,255,255)
 O = (0,0,0)
 P = (255,105, 180)
-    
-    
+K = (1,1,1)    
+OR = (226,119,31)    
     
     
 pet1 = [
-	O, O, O, O, O, O, O, O,
-	P, O, O, O, O, O, O, O,
-	O, P, O, O, P, O, P, O,
-	O, P, G, G, P, Y, Y, O,
-	O, G, G, G, Y, W, Y, G,
-	O, G, G, G, G, Y, Y, O,
-	O, G, O, G, O, G, O, O,
-	O, O, O, O, O, O, O, O,
-	]
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, R, R, R, R, R, R, O,
+        O, R, R, R, R, R, R, O,
+        O, K, K, O, K, K, O, O,
+        ]
+
+
+	        
+
 pet2 = [
 	O, O, O, O, O, O, O, O,
 	P, O, O, O, O, O, O, O,
@@ -34,45 +38,23 @@ pet2 = [
 	O, O, O, O, O, O, O, O,
 	]
 
-def walking():
+RED = [
+	K, R, R, R, R, R, R, K,
+	R, K, R, R, R, R, K, R,
+	R, R, K, R, R, K, R, R,
+	R, R, R, K, K, R, R, R,
+	R, R, R, K, K, R, R, R,
+	R, R, K, R, R, K, R, R,
+	R, K, R, R, R, R, K, R,
+	K, R, R, R, R, R, R, K,
+        ]
 
+
+
+while True:
 	sense.set_pixels(pet1)
-	time.sleep(0.5)
-	sense.set_pixels(pet2)
-	time.sleep(0.5)
-  	while x<.05 or x>.05 or y<.05 or y>.05 or z<.05 or z>.05:
-
-	        RED = [
-        	        R, R, R, R, R, R, R, R,
-        	        R, R, R, R, R, R, R, R,
-        	        R, R, R, R, R, R, R, R,
-        	        R, R, R, R, R, R, R, R,
-                	R, R, R, R, R, R, R, R,
-			R, R, R, R, R, R, R, R,
-        	        R, R, R, R, R, R, R, R,
-        	        R, R, R, R, R, R, R, R,
-        	        ]
-
-
-  
-sense.clear()
-
-x, y, z = sense.get_accelerometer_raw().values()
-
-while x<2 and y<2 and z<2:
 	x, y, z = sense.get_accelerometer_raw().values()
-	walking()
-
-while x<0 or x>0 or y<0 or y>0 or z<0 or z>0:
-	
-	RED = [
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-        	R, R, R, R, R, R, R, R,
-	        ]
-
+	if(x>1 or y>1 or z>1):
+		sense.set_pixels(RED)
+	time.sleep(1)
+sense.clear
